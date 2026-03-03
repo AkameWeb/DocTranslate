05c374d7019ad015a8a728911b681ce637846928
AkiNo <mejkrejden@gmail.com>
2026-03-03 23:07:01 +0400

    03.03.2026 Google-Cloud

--- google-config.php
+++ google-config.php
@@ -1,4 +1,4 @@
 <?php
-$clientID = '1070327749176-jkbh0i4denc4040qc9op6o90sb7vlumj.apps.googleusercontent.com';
-$clientSecret = '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';
+$clientID = '';
+$clientSecret = '';
 $redirectUri = 'http://localhost/DocLang/google-callback.php';
